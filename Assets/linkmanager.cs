7fbe497d848e431227532662948b8007b869c0ea
Manoj M <manoj2003nov@gmail.com>
2023-11-21 12:55:46 +0530

    Initial commit

---
+++ Assets/linkmanager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class linkmanager: MonoBehaviour
+{
+       public void openvolleyball()
+    {
+        Application.OpenURL("https://youtube.com/watch?v=hLZ44KBWEbo&feature=share7");
+    }
+    public void openfootball()
+    {
+        Application.OpenURL("https://youtube.com/watch?v=dFLaabgXhpc&feature=share7");
+    }
+    public void opencricket()
+    {
+        Application.OpenURL("https://youtube.com/watch?v=M5MD5i6VpYQ&feature=share7");
+    }
+   
+
+}
---
+++ Assets/rotate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public Vector3 rot;
+    
+
+    void Update()
+    {
+        transform.Rotate(rot*Time.deltaTime);
+    }
+}
